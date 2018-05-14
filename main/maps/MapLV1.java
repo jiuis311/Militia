f6f03691257f78b224c5f2a5ced63b214f3fd5f0
TheVu <thevudoan@gmail.com>
2018-05-14 15:08:03 +0700

    remove board

--- main/maps/MapLV1.java
+++ main/maps/MapLV1.java
@@ -12,32 +12,26 @@ public class MapLV1 extends Map {
      
     public MapLV1() {
         super();
-        //board[1][1] = Symbol.MINION;
         monsters.add(new Minion(new Position(1, 1)));
         
-        //board[1][3] = Symbol.MINION;
         monsters.add(new Minion(new Position(1, 3)));
         
-        //board[7][7] = Symbol.MINION;
         monsters.add(new Minion(new Position(7, 7)));
         
-        //board[4][7] = Symbol.MINION;
         monsters.add(new Minion(new Position(4, 7)));
         
-        //board[1][6] = Symbol.SWORD;
-        heroes.add(new Swordman(new Position(1, 6)));
-        
-        //board[5][2] = Symbol.BIG_MINION;
         monsters.add(new BigMinion(new Position(5, 2)));
         
-        //board[2][4] = Symbol.BIG_MINION;
         monsters.add(new BigMinion(new Position(2, 4)));
         
         items.add(new Bomb(new Position(6, 2)));
         
         items.add(new Shield(new Position(2, 6)));
-        //board[6][4] = Symbol.SPEAR;
+
         heroes.add(new Lancer(new Position(6, 4)));
+        
+        heroes.add(new Swordman(new Position(1, 6)));
+        
         setTurns(4);
         setTargetedMons(6);
         for (Monster mons:monsters) {
